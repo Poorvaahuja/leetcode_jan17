a771899739082f5f1b844b5e378ae36c64eebd86
Poorvaahuja <122693422+Poorvaahuja@users.noreply.github.com>
2024-01-17 21:35:29 +0530

    Create Q2_solution.java

---
+++ Q2_solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int incremovableSubarrayCount(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(isIncreasing(nums, i, j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public boolean isIncreasing(int[] arr, int st, int end){
+        int n = arr.length; 
+        int prev = 0;
+        for(int i=0; i<n; i++){
+            if(i<= end && i>= st){
+                continue;
+            }
+            if(arr[i] <= prev){
+                return false;
+            }
+            prev = arr[i];
+        }
+        return true;
+    }
+}
